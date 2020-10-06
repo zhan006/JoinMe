ed47800e745a75dcb13d528c8544c7b736fd5cc5
zhan006 <zhan006@ntu.edu.sg>
2020-10-06 15:21:38 +1100

    profile page finalised

--- app/src/main/java/com/example/joinme/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/joinme/fragments/ProfileFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +16,54 @@ import androidx.fragment.app.Fragment;
 import com.example.joinme.R;
 
 public class ProfileFragment extends Fragment {
+    public static final int ALBUM_DISPLAY=3, FRIEND_DISPLAY=3;
+    private TextView aboutMe,name,location;
+    private ImageButton addAlbum;
+    LinearLayout friendGallery,albums;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup)inflater.inflate(R.layout.activity_profile,container,false);
+        aboutMe = view.findViewById(R.id.aboutMe);
+        friendGallery = view.findViewById(R.id.friend_gallery);
+        albums = view.findViewById(R.id.albums);
+        addAlbum=view.findViewById(R.id.addAlbum);
+        name = view.findViewById(R.id.name);
+        location = view.findViewById(R.id.location);
+        addAlbum.setOnClickListener((v)->{
+            addAlbum(R.drawable.default_icon);
+        });
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        addAlbum(R.drawable.default_icon);
+    }
+    public void setName(String name){
+        this.name.setText(name);
+    }
+    public void setLocation(String location){
+        this.location.setText(location);
+    }
+    public void setAboutMe(String text){
+        aboutMe.setText(text);
+    }
+    public void addAlbum(int image){
+        if(albums.getChildCount()<ALBUM_DISPLAY){
+            ImageView v= new ImageView(getContext());
+            v.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+            v.setImageResource(image);
+            albums.addView(v);
+        }
+    }
+    public void addFriends(int image){
+        if(albums.getChildCount()<FRIEND_DISPLAY){
+            ImageView v= new ImageView(getContext());
+            v.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+            v.setImageResource(image);
+            albums.addView(v);
+        }
     }
 }
--- app/src/main/java/com/example/joinme/reusableComponent/NavBar.java
+++ app/src/main/java/com/example/joinme/reusableComponent/NavBar.java
@@ -1,30 +1,16 @@
 package com.example.joinme.reusableComponent;
 
-import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
-import android.media.Image;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import com.example.joinme.MainActivity;
 import com.example.joinme.R;
 import com.example.joinme.fragments.ProfileFragment;
-import com.example.joinme.profileActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.zip.Inflater;
 
 public class NavBar extends LinearLayout {
     private BottomNavigationView nav;
@@ -38,7 +24,8 @@ public class NavBar extends LinearLayout {
         nav.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.tab_account:
-                    manager.beginTransaction().replace(R.id.main_fragment_container,new ProfileFragment()).commit();
+                    ProfileFragment pro_frag = new ProfileFragment();
+                    manager.beginTransaction().replace(R.id.main_fragment_container,pro_frag).commit();
                     break;
             }
             return true;
