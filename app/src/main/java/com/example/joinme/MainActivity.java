6e8df559efa3ce3af19619ea26a16f260bdeced6
Tong Su <tsu2@student.unimelb.edu.au>
2020-10-11 18:30:14 +0800

    firebase testing demo

--- app/src/main/java/com/example/joinme/MainActivity.java
+++ app/src/main/java/com/example/joinme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joinme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -8,12 +9,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
 
+import com.example.joinme.database.FirebaseAPI;
 import com.example.joinme.reusableComponent.NavBar;
 import com.example.joinme.reusableComponent.TitleBar;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +35,32 @@ public class MainActivity extends AppCompatActivity {
         NavBar nav = (NavBar) findViewById(R.id.navbar);
         nav.setSelectedItem(R.id.tab_home);
 
+        /*
+         * Firebase testing demo
+         */
+        FirebaseAPI.setFirebaseData("User/qa6KACdJ0RYZfVDXLtpKL2HcxJ43/username", "Giovanna")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
 
+            }
+        });
 
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                String username = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "onDataChange: username = "+username);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+
+        FirebaseAPI.getFirebaseData("User/qa6KACdJ0RYZfVDXLtpKL2HcxJ43/username", userListener);
     }
 }
---
+++ app/src/main/java/com/example/joinme/database/FirebaseAPI.java
@@ -0,0 +1,66 @@
+package com.example.joinme.database;
+
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Encapsulates methods for accessing Firebase
+ */
+public class FirebaseAPI {
+
+    private static final String TAG = "FirebaseAPI";
+
+    // root
+    public static DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    // User
+    public static DatabaseReference userRef = rootRef.child("User");
+
+    // Firebase auth
+    public static FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    public static void getFirebaseData(String nodePath, ValueEventListener valueEventListener) {
+        DatabaseReference dbRef = rootRef.child(nodePath);
+        // Read from the database
+        dbRef.addValueEventListener(valueEventListener);
+    }
+
+    public static Task<Void> setFirebaseData(String nodePath, String newData) {
+        return rootRef.child(nodePath).setValue(newData);
+    }
+
+    /**
+     * Push to a given Firebase node
+     * @return unique key
+     */
+    public static String addFirebaseData(String nodePath, Object data, OnCompleteListener completionListener) {
+        String key = rootRef.child(nodePath).push().getKey();
+        rootRef.child(nodePath+"/"+key).setValue(data).addOnCompleteListener(completionListener);
+        return key;
+    }
+
+
+    public static Task<AuthResult> login(String email, String password) {
+        return auth.signInWithEmailAndPassword(email,password);
+    }
+
+    public static Task<AuthResult> signUp(String email, String password) {
+        return auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    public static FirebaseUser getUser() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        Log.d(TAG, "getUser UID : " + currentUser.getUid());
+        return currentUser;
+    }
+
+
+
+}
