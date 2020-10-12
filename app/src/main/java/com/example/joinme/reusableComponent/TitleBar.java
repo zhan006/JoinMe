aae6eedff28b3e42256b2cdf95d088dfc6f72850
Tong Su <tsu2@student.unimelb.edu.au>
2020-10-12 22:13:56 +0800

    add common icon for title bar

--- app/src/main/java/com/example/joinme/reusableComponent/TitleBar.java
+++ app/src/main/java/com/example/joinme/reusableComponent/TitleBar.java
@@ -2,6 +2,8 @@ package com.example.joinme.reusableComponent;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,7 +16,10 @@ import androidx.annotation.Nullable;
 
 import com.example.joinme.R;
 
+import java.lang.ref.Reference;
+
 public class TitleBar extends LinearLayout {
+    private static final String TAG = "TitleBar";
     private AttributeSet attrs;
     public TitleBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -22,11 +27,19 @@ public class TitleBar extends LinearLayout {
         LayoutInflater.from(context).inflate(R.layout.titlebar,this);
         String newTitle = attrs.getAttributeValue("http://schemas.android.com/apk/res-auto","title");
         String backable = attrs.getAttributeValue("http://schemas.android.com/apk/res-auto","backable");
+
+        Drawable icon;
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TitleBar);
+        icon = ta.getDrawable(R.styleable.TitleBar_icon);
+
         ImageButton back = (ImageButton) this.findViewById(R.id.backBtn);
+        ImageButton iconBtn = findViewById(R.id.icon_btn);
         setTitle(newTitle);
-        if(backable.equals("false")){
+        setIcon(iconBtn, icon);
+
+        if (backable.equals("false")){
             back.setVisibility(INVISIBLE);
-        }else{
+        } else{
             back.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -34,9 +47,14 @@ public class TitleBar extends LinearLayout {
                 }
             });
         }
+
     }
     public void setTitle(String title){
         TextView text = (TextView)findViewById(R.id.title);
         text.setText(title);
+    }
+
+    public void setIcon(ImageButton iconBtn, Drawable icon) {
+        iconBtn.setImageDrawable(icon);
     }
 }
