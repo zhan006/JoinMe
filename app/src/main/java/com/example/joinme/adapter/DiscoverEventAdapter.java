0b9be115c82268cd43a8a803fa7aa9787fe647f7
WangRRRui <wr19960611@gmail.com>
2020-11-05 19:00:28 +1100

    update

--- app/src/main/java/com/example/joinme/adapter/DiscoverEventAdapter.java
+++ app/src/main/java/com/example/joinme/adapter/DiscoverEventAdapter.java
@@ -3,13 +3,10 @@ package com.example.joinme.adapter;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,29 +20,26 @@ import com.example.joinme.fragments.EventDetailFragment;
 import com.example.joinme.objects.Event;
 import com.example.joinme.utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
+
+//This adapter is to manage the RecyclerView for the discoverEventFragment.
 public class DiscoverEventAdapter extends RecyclerView.Adapter<DiscoverEventAdapter.ViewHolder> {
-    private List<Event> eventList; //Hold all events for discovering in the database
+    private final List<Event> eventList; //Hold all events for discovering in the database
     private static Location loc; //Hold current location
 
     public DiscoverEventAdapter(List<Event> eventList, Location l) {
         this.eventList = eventList;
-        this.loc = l;
+        loc = l;
     }
 
-    public Location getLoc(){
-        return loc;
-    }
-
+    //Inner class holding the view
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView title,time,address;
         Button detail;
         Button join;
         @RequiresApi(api = Build.VERSION_CODES.N)
-        public ViewHolder(@NonNull View itemView, DiscoverEventAdapter adapter) {
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.event_name);
             time = (TextView) itemView.findViewById(R.id.event_datetime);
@@ -53,13 +47,6 @@ public class DiscoverEventAdapter extends RecyclerView.Adapter<DiscoverEventAdap
             detail = (Button)itemView.findViewById(R.id.detail_button);
             join = (Button)itemView.findViewById(R.id.join_button);
             join.setVisibility(View.GONE);
-            /*
-            detail.setOnClickListener((v)->{
-
-            });
-            join.setOnClickListener((v)->{
-
-            });*/
         }
     }
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -67,21 +54,18 @@ public class DiscoverEventAdapter extends RecyclerView.Adapter<DiscoverEventAdap
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.discover_event_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view,this);
+        ViewHolder viewHolder = new ViewHolder(view);
+        //Click the detail button to go to the event detail page
+        viewHolder.detail.setOnClickListener(view1 -> {
+            int position = viewHolder.getAdapterPosition();
+            Event currentEvent1 = eventList.get(position);
+            Toast.makeText(view1.getContext(), "You clicked on detail button of " + currentEvent1.getEventName(), Toast.LENGTH_SHORT).show();
+            EventDetailFragment f = new EventDetailFragment();
+            Bundle bd = new Bundle();
+            bd.putSerializable("current_event", currentEvent1);
+            f.setArguments(bd);
+            utils.replaceFragment(((AppCompatActivity) view1.getContext()).getSupportFragmentManager(),f, null);
 
-        viewHolder.detail.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                int position = viewHolder.getAdapterPosition();
-                Event currentEvent1 = eventList.get(position);
-                Toast.makeText(view.getContext(), "You clicked on detail button of " + currentEvent1.getEventName(), Toast.LENGTH_SHORT).show();
-                EventDetailFragment f = new EventDetailFragment();
-                Bundle bd = new Bundle();
-                bd.putSerializable("current_event", currentEvent1);
-                f.setArguments(bd);
-                utils.replaceFragment(((AppCompatActivity)view.getContext()).getSupportFragmentManager(),f, null);
-
-            }
         });
         return viewHolder;
     }
@@ -91,6 +75,7 @@ public class DiscoverEventAdapter extends RecyclerView.Adapter<DiscoverEventAdap
         Event event = eventList.get(position);
         ((ViewHolder) holder).title.setText(event.getEventName());
         if(event.getDatetime()!=null) ((ViewHolder) holder).time.setText(event.getDatetime().toString());
+        //show location and distance.
         if(event.getLocation()!=null) ((ViewHolder) holder).address.setText(
                 event.getLocation().getAddress()+"       "+
                         String.format("%.2f", (Math.round(event.getLocation().distanceTo(loc))/1000.0))+" km");
@@ -101,7 +86,6 @@ public class DiscoverEventAdapter extends RecyclerView.Adapter<DiscoverEventAdap
     public int getItemCount() {
         return eventList.size();
     }
-
 
 }
 
